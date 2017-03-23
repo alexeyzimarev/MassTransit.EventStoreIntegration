0335bb3b1ea24400bd48f36ce75dc3bb4b00f098
Alexey Zimarev <alzi@abax.no>
2017-03-23 17:06:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# MassTransit.EventStore
+EventStore audit store for MassTransit
+
+[![NuGet](https://img.shields.io/nuget/v/MassTransit.EventStore.svg)](https://www.nuget.org/packages/MassTransit.EventStore/)
+
+## Installation
+
+The library is published on nuget.org.
+
+Use `Install-Package MassTransit.EventStore` to install it.
+
+## Usage
+
---
+++ src/MassTransit.EventStoreIntegration/EventStoreMessageAudit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using EventStore.ClientAPI;
+using MassTransit.Audit;
+using MassTransit.Util;
+using Newtonsoft.Json;
+
+namespace MassTransit.EventStoreIntegration
+{
+    public class EventStoreMessageAudit : IMessageAuditStore
+    {
+        private readonly IEventStoreConnection _connection;
+        private readonly string _auditStreamName;
+
+        public EventStoreMessageAudit(IEventStoreConnection connection, string auditStreamName)
+        {
+            _connection = connection;
+            _auditStreamName = auditStreamName;
+        }
+
+        public async Task StoreMessage<T>(T message, MessageAuditMetadata metadata) where T : class
+        {
+            var auditEvent = new EventData(Guid.NewGuid(), TypeMetadataCache<T>.ShortName,
+                true, Serialise(message), Serialise(metadata));
+            await _connection.AppendToStreamAsync(_auditStreamName, ExpectedVersion.Any, auditEvent)
+                .ConfigureAwait(false);
+        }
+
+        private static byte[] Serialise(object @event)
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    JsonSerializer.CreateDefault().Serialize(writer, @event);
+                    writer.Flush();
+                }
+                return stream.ToArray();
+            }
+        }
+    }
+}
